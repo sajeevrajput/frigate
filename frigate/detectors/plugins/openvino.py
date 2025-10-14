acb4747b3b61e0353341b819b54ca9eaf8c666ca
Y <sajeev.rajput@intel.com>
2025-10-14 08:46:34 +0530

    add support for other detectors

--- frigate/detectors/plugins/openvino.py
+++ frigate/detectors/plugins/openvino.py
@@ -30,7 +30,8 @@ class OvDetectorConfig(BaseDetectorConfig):
 
 
 class OvDetector(DetectionApi):
-    type_key = "DETECTOR_KEY" # disabling this class from loading in api_types
+    # type_key = DETECTOR_KEY # disabling this class from loading in api_types
+    type_key = "DETECTOR_KEY" # disabling this class from loading in api_typess
     supported_models = [
         ModelTypeEnum.dfine,
         ModelTypeEnum.rfdetr,
@@ -239,6 +240,7 @@ class OvDetector(DetectionApi):
     
 
 class OvAsyncDetector(DetectionApi):
+    # type_key = "DETECTOR_KEY"
     type_key = DETECTOR_KEY
     supported_models = [
         ModelTypeEnum.dfine,
@@ -281,6 +283,65 @@ class OvAsyncDetector(DetectionApi):
                 f"OpenVino detector does not support {self.ov_model_type} models."
             )
             self.model_invalid = True
+            
+    
+        if self.ov_model_type == ModelTypeEnum.ssd:
+            model_inputs = self.interpreter.inputs
+            model_outputs = self.interpreter.outputs
+
+            if len(model_inputs) != 1:
+                logger.error(
+                    f"SSD models must only have 1 input. Found {len(model_inputs)}."
+                )
+                self.model_invalid = True
+            if len(model_outputs) != 1:
+                logger.error(
+                    f"SSD models must only have 1 output. Found {len(model_outputs)}."
+                )
+                self.model_invalid = True
+
+            output_shape = model_outputs[0].get_shape()
+            if output_shape[0] != 1 or output_shape[1] != 1 or output_shape[3] != 7:
+                logger.error(f"SSD model output doesn't match. Found {output_shape}.")
+                self.model_invalid = True
+
+        if self.ov_model_type == ModelTypeEnum.yolonas:
+            model_inputs = self.interpreter.inputs
+            model_outputs = self.interpreter.outputs
+
+            if len(model_inputs) != 1:
+                logger.error(
+                    f"YoloNAS models must only have 1 input. Found {len(model_inputs)}."
+                )
+                self.model_invalid = True
+            if len(model_outputs) != 1:
+                logger.error(
+                    f"YoloNAS models must be exported in flat format and only have 1 output. Found {len(model_outputs)}."
+                )
+                self.model_invalid = True
+            output_shape = model_outputs[0].partial_shape
+            if output_shape[-1] != 7:
+                logger.error(
+                    f"YoloNAS models must be exported in flat format. Model output doesn't match. Found {output_shape}."
+                )
+                self.model_invalid = True
+
+        if self.ov_model_type == ModelTypeEnum.yolox:
+            self.output_indexes = 0
+            while True:
+                try:
+                    tensor_shape = self.interpreter.output(self.output_indexes).shape
+                    logger.info(
+                        f"Model Output-{self.output_indexes} Shape: {tensor_shape}"
+                    )
+                    self.output_indexes += 1
+                except Exception:
+                    logger.info(f"Model has {self.output_indexes} Output Tensors")
+                    break
+            self.num_classes = tensor_shape[2] - 5
+            logger.info(f"YOLOX model has {self.num_classes} classes")
+            self.calculate_grids_strides()
+
         self.infer_thread = threading.Thread(target=self.inference_runner, daemon=True)
         self.infer_thread.start()
             
@@ -303,6 +364,12 @@ class OvAsyncDetector(DetectionApi):
         #     self.infer_queue.wait_all()
           
     def callback(self, infer_request, userdata):
+        
+        detections = np.zeros((20, 6), np.float32)
+
+        if self.model_invalid:
+            return detections
+        
         # adding for yologeneric only atm
         output_tensor = infer_request.get_output_tensor(0).data
         # post process
@@ -312,6 +379,77 @@ class OvAsyncDetector(DetectionApi):
             for item in output_tensor:
                 out_tensor.append(item.data)
             processed_output = post_process_yolo(out_tensor, self.w, self.h)
+            
+        elif self.ov_model_type == ModelTypeEnum.rfdetr:
+            processed_output =  post_process_rfdetr(
+                [
+                    infer_request.get_output_tensor(0).data,
+                    infer_request.get_output_tensor(1).data,
+                ]
+            )
+        elif self.ov_model_type == ModelTypeEnum.ssd:
+            results = infer_request.get_output_tensor(0).data[0][0]
+
+            for i, (_, class_id, score, xmin, ymin, xmax, ymax) in enumerate(results):
+                if i == 20:
+                    break
+                detections[i] = [
+                    class_id,
+                    float(score),
+                    ymin,
+                    xmin,
+                    ymax,
+                    xmax,
+                ]
+            processed_output = detections
+        elif self.ov_model_type == ModelTypeEnum.yolonas:
+            predictions = infer_request.get_output_tensor(0).data
+
+            for i, prediction in enumerate(predictions):
+                if i == 20:
+                    break
+                (_, x_min, y_min, x_max, y_max, confidence, class_id) = prediction
+                # when running in GPU mode, empty predictions in the output have class_id of -1
+                if class_id < 0:
+                    break
+                detections[i] = [
+                    class_id,
+                    confidence,
+                    y_min / self.h,
+                    x_min / self.w,
+                    y_max / self.h,
+                    x_max / self.w,
+                ]
+            processed_output = detections
+            
+        elif self.ov_model_type == ModelTypeEnum.yolox:
+            out_tensor = infer_request.get_output_tensor()
+            # [x, y, h, w, box_score, class_no_1, ..., class_no_80],
+            results = out_tensor.data
+            results[..., :2] = (results[..., :2] + self.grids) * self.expanded_strides
+            results[..., 2:4] = np.exp(results[..., 2:4]) * self.expanded_strides
+            image_pred = results[0, ...]
+
+            class_conf = np.max(
+                image_pred[:, 5 : 5 + self.num_classes], axis=1, keepdims=True
+            )
+            class_pred = np.argmax(image_pred[:, 5 : 5 + self.num_classes], axis=1)
+            class_pred = np.expand_dims(class_pred, axis=1)
+
+            conf_mask = (image_pred[:, 4] * class_conf.squeeze() >= 0.3).squeeze()
+            # Detections ordered as (x1, y1, x2, y2, obj_conf, class_conf, class_pred)
+            detections = np.concatenate(
+                (image_pred[:, :5], class_conf, class_pred), axis=1
+            )
+            detections = detections[conf_mask]
+
+            ordered = detections[detections[:, 5].argsort()[::-1]][:20]
+
+            for i, object_detected in enumerate(ordered):
+                detections[i] = self.process_yolo(
+                    object_detected[6], object_detected[5], object_detected[:4]
+                )
+            processed_output = detections
 
         self.response_store.put(userdata, processed_output)
         
